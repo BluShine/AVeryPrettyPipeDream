fc7c39fb4850ab735e4515f01f7a0e249856fc82
Mark <umbreon27@gmail.com>
2016-12-05 03:04:43 -0800

    Lock cursor and remove unused inputs.
    Added cursor locking. Removed unsued inputs from input manager.

---
+++ Assets/scripts/CursorLocker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorLocker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        if(Input.GetButtonDown("Cancel"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+}
