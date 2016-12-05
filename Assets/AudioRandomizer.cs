7fe146156a8d951ecf9fcc6fdd663641a2d59bd3
Mark <umbreon27@gmail.com>
2016-12-05 00:17:17 -0800

    Added ambient sounds.
    Added ambient sounds for the initial dream, classroom, and each weather
    condition. All sounds are CC0 taken from freesound.

---
+++ Assets/AudioRandomizer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioRandomizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource a = GetComponent<AudioSource>();
+        a.time = Random.Range(0f, a.clip.length);
+	}
+}
